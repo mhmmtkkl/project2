c41b1d0adf14c0f8073f08d57b9496fe9742e48c
mhmmtkkl <makoklu32@gmail.com>
2020-04-08 11:11:30 -0400

    Project2

--- src/maxNumber.java
+++ src/maxNumber.java
@@ -12,20 +12,6 @@ public class maxNumber {
 
     public static void main(String[] args) {
 
-        int[][] i1 = new int[][]{{1,2,3}, {2,3,1} , {5,5,5} , {2,10,3}};
-
-        int max = 0 ;
-        for( int i = 0 ; i < i1.length ; i++){
-            for(int j =0 ; j< i1[i].length ; j++){
-
-                if(i1[i][j] > max){
-                    max = i1[i][j];
-                }
-            }
-
-        }
-
-        System.out.println(max);
 
     }
 
